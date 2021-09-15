53ce2d7d8ef2003bdc47063dc991d557c4200df2
Reece <reece.jm61@gmail.com>
2021-09-15 15:26:23 -0500

    added components/services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponentsComponent } from './components/game/game-components/game-components.component';
+import { ComponentsComponent } from './components/components.component';
+import { GameComponent } from './components/game/game.component';
+import { GameButtonsComponent } from './components/game/game-buttons/game-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponentsComponent,
+    ComponentsComponent,
+    GameComponent,
+    GameButtonsComponent
   ],
   imports: [
     BrowserModule
