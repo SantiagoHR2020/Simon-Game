70735ef0dcc90e2164078ce9e628de2f4df67640
bwoods97 <bwoods97@charter.net>
2021-09-16 17:44:17 -0400

    Stylesheet update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GameStateService } from './services/game-state.service';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
 
   ],
   imports: [
